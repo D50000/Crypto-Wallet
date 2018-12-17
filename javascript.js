cdc3cc741e4bcd694dd2d0daa6f26cf7331ba5a8
Eddie Hsu <aoe102198@gmail.com>
2018-12-17 15:02:45 +0800

    update
    fix myCrytoCheck() bug

--- javascript.js
+++ javascript.js
@@ -142,7 +142,7 @@ function myCrytoCheck() {
     let li = ul.getElementsByTagName("li");
     for (let i = 0; i < li.length; i++) {
         let a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        if (a.textContent.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "block";
         } else {
             li[i].style.display = "none";
